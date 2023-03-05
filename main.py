5186d978b2e95ab48288920f4b8ac424b9f37937
Tgeese3 <ogasawara.mizukami.aimr.nanase@gmail.com>
2023-03-05 17:55:02 +0900

    new test of streamlit

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy  as np
+import pandas as pd
+from PIL import Image
+import time
+
+st.title('streamlit Begin')
+st.write('Display image')
+
+'start!'
+
+latest_iteration=st.empty()
+bar=st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+'Done!'
+
+if st.checkbox('Show Image'):
+    img = Image.open('influencer.jpg')
+    st.image(img, caption='Nogizaka 46', use_column_width=True  )
